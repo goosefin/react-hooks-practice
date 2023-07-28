873aba91a160767d06a555710ad4ec67d2a52218
goosefin <agustinafin96@gmail.com>
2023-07-28 18:30:05 -0400

    ternary conditioning practice

--- src/components/ConditionalComponent.js
+++ src/components/ConditionalComponent.js
@@ -3,13 +3,17 @@ import Counter from './Counter'
 import FunctionalCounter from './FunctionalCounter'
 
 export default function ConditionalComponent(){
-    const [display,setDisplay] = useState(false)
-    let output
+    const [display,setDisplay] = useState(true)
+    // let output
 
-    if(display){
-        output = <h3>This is a conditional component</h3>
-    }else{
-        output = <h3>This is ALSO a conditional component</h3>
-    }
-    return <>{output}</>
+    //   is this true ? what to do if yes : what to do if no
+    return display ? (
+        <div>
+            <h3>This is a conditional component</h3>
+        </div>
+    ) : (
+        <div> 
+            <h3>This is ALSO a conditional component</h3>
+        </div>
+    )
 }
